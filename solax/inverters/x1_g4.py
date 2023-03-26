557f0c904921c4e4ac3b8b4b66c2acc6e9975ba7
Robohelo <59528552+Robohelo@users.noreply.github.com>
2023-03-26 14:56:29 +0200

    Update x1_g4.py

--- solax/inverters/x1_g4.py
+++ solax/inverters/x1_g4.py
@@ -1,6 +1,7 @@
 import voluptuous as vol
 
-from solax.inverter import Inverter
+from solax import utils
+from solax.inverter import Inverter, InverterHttpClient, Method, ResponseParser
 from solax.units import Total, Units
 from solax.utils import startswith
 from solax.utils import div10, div100, to_signed
@@ -23,6 +24,17 @@ class X1G4(Inverter):
             vol.Required("Information"): vol.Schema(vol.All(vol.Length(min=10, max=10))),
         }, extra=vol.REMOVE_EXTRA
     )
+
+    @classmethod
+    def _build(cls, host, port, pwd="", params_in_query=False):
+        url = utils.to_url(host, port)
+        http_client = InverterHttpClient(url, Method.POST, pwd)
+        http_client.with_default_data()
+
+        schema = cls.schema()
+        response_decoder = cls.response_decoder()
+        response_parser = ResponseParser(schema, response_decoder)
+        return cls(http_client, response_parser)
 
     @classmethod
     def response_decoder(cls):
