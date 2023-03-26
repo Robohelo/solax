a19e6a3acd415c5405136e68ffdd80e9106ffbc7
Robohelo <59528552+Robohelo@users.noreply.github.com>
2023-03-26 16:46:35 +0200

    Update x1_g4.py

--- solax/inverters/x1_g4.py
+++ solax/inverters/x1_g4.py
@@ -8,22 +8,24 @@ from solax.utils import div10, div100, to_signed
 
 class X1G4(Inverter):
     # pylint: disable=duplicate-code
-    _schema = vol.Schema(
-        {
-            vol.Required("type"): vol.All(str, startswith("X1-")),
-            vol.Required("SN"): str,
-            vol.Required("ver"): str,
-            vol.Required("Data"): vol.Schema(
-                vol.All(
-                    [vol.Coerce(float)],
-                    vol.Any(
-                        vol.Length(min=100, max=100),
-                    ),
+    _schema = vol.Schema({
+        vol.Required('sn'): str,
+        vol.Required('ver'): str,
+        vol.Required('type'): int,
+        vol.Required('Data'): vol.Schema(
+            vol.All(
+                [vol.Coerce(float)],
+                vol.Any(
+                    vol.Length(min=100, max=100),
+                ),
+            )
+        ),
+        vol.Required('Information'): vol.Schema(
+            vol.All(
+                vol.Length(min=10, max=10)
                 )
             ),
-            vol.Required("Information"): vol.Schema(vol.All(vol.Length(min=10, max=10))),
-        }, extra=vol.REMOVE_EXTRA
-    )
+    }, extra=vol.REMOVE_EXTRA)
 
     @classmethod
     def _build(cls, host, port, pwd="", params_in_query=False):
